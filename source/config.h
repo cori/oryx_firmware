e56ff22519b615fef635769eebaf4d947b6edceb
cori <cori@glitch.com>
2023-05-01 23:13:46 -0500

    LB6Y0 - trying to match the last hand-rolled tapping config - tap term 225, no ignore_mod_tap, and retro_tap

--- source/config.h
+++ source/config.h
@@ -3,9 +3,13 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define IGNORE_MOD_TAP_INTERRUPT
+#undef TAPPING_TERM
+#define TAPPING_TERM 225
+
+#define RETRO_TAPPING
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"z9xWM/0DY4P"
+#define FIRMWARE_VERSION u8"z9xWM/LB6Y0"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
